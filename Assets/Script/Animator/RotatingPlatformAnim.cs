ec43e865f64614c15e36117b98efd130bc890786
isincic-98 <i.sincic@nouvellesimages.xyz>
2026-04-24 16:52:31 +0200

    Modified rotating platform

--- Assets/Script/Animator/RotatingPlatformAnim.cs
+++ Assets/Script/Animator/RotatingPlatformAnim.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 public class RotatingPlatformAnim : MonoBehaviour
 {
     [SerializeField] private float _animDuration;
+    [SerializeField] private float _orientation;
+
+    [SerializeField] private float _rotationOffset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.transform.DORotate(new Vector3(450f, 0f, 0f), _animDuration, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+        transform.DORotate(new Vector3(360f * _orientation, _rotationOffset, 0), _animDuration, RotateMode.LocalAxisAdd).SetEase(Ease.Linear)
+            .SetLoops(-1);
     }
 
     // Update is called once per frame
     void Update()
     {
     }
+
 }
